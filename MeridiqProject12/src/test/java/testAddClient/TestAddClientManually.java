cc500e454f1c15c1037ac84ba08be3f6359a2d8b
shreyal <shreyal.kodecreators@gmail.com>
2026-02-20 17:02:02 +0530

    Update test add client manually details

--- MeridiqProject12/src/test/java/testAddClient/TestAddClientManually.java
+++ MeridiqProject12/src/test/java/testAddClient/TestAddClientManually.java
@@ -68,7 +68,7 @@ public class TestAddClientManually
 		Thread.sleep(3000);
 		
 		driver.findElement(By.xpath("//div[text() = 'Submit']")).click();
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 		
 	}
 	
